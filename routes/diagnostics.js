3d9cdd17778095a94c937c89a1ddd9d1af52c607
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-27 11:30:31 -0700

    add function

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,15 +1,35 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
+diagnostics.get("/", (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile("./db/diagnostic.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
 });
 
 // POST Route for a error logging
-diagnostics.post('/', (req, res) => {
+diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body);
+
+  const { username, topic, tip } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      username,
+      tip,
+      topic,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newTip, "./db/diagnostic.json");
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error("Error in adding a tip");
+  }
 });
 
 module.exports = diagnostics;
